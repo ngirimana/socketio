04db321f0b04924c86644101d017188aad08bc5b
ngirimana <chadrackngirimana@gmail.com>
2021-01-20 16:32:22 +0200

    port

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express=require('express');
-const app=express();
-const server=require('http').createServer(app);
-const io=require('socket.io')(server);
-
-//Reserved Events
-let ON_CONNECTION='connection';
-let ON_DISCONNECT='disconnect';
-
-//main events
-let EVENT_IS_USER_ONLINE="check_online";
-let EVENT_SINGLE_CHAT_MESSAGE="single_chat-message";
-
-//sub events
-let SUB_EVENT_RECEIVE_MESSAGE='receive_message';
-let SUB_EVENT_IS_USER_CONNECTED="is_user_connected";
+// app.js
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+// Reserved Events
+let ON_CONNECTION = 'connection';
+let ON_DISCONNECT = 'disconnect';
+// Main Events
+let EVENT_IS_USER_ONLINE = 'check_online';
+let EVENT_SINGLE_CHAT_MESSAGE = 'single_chat_message';
+// Sub Events
+let SUB_EVENT_RECEIVE_MESSAGE = 'receive_message';
+let SUB_EVENT_MESSAGE_FROM_SERVER = 'message_from_server';
+let SUB_EVENT_IS_USER_CONNECTED = 'is_user_connected';
 
 // status
 const  STATUS_MESSAGE_NOT_SENT = 1001;
@@ -24,9 +23,9 @@ app.use('/',(req,res)=>{
 	res.send('Server is running');
 })
 const userMap=new Map();
-io.sockets.on(ON_CONNECTION,()=>{
+io.sockets.on(ON_CONNECTION, function (socket) {
 	onEachUserConnection(socket);
-});
+   });
 const onEachUserConnection=(socket)=>{
 print('----------------------');
 print(`Connected => Socket ID': ${socket.id}, User: ${stringfyToJson(socket.handshake.query)}`);
