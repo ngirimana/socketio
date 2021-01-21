af4edbd83f256d71c044b54b8ac7f08f964e8c97
ngirimana <chadrackngirimana@gmail.com>
2021-01-21 15:39:50 +0200

    documenting

--- server.js
+++ server.js
@@ -3,29 +3,41 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-// Reserved Events
+/**
+ * Reserved Events
+ */
+
 const ON_CONNECTION = 'connection';
 const ON_DISCONNECT = 'disconnect';
-// Main Events
-let EVENT_IS_USER_ONLINE = 'check_online';
-let EVENT_SINGLE_CHAT_MESSAGE = 'single_chat_message';
-// Sub Events
+/**
+ * Main Events
+ */
+
+const EVENT_IS_USER_ONLINE = 'check_online';
+const EVENT_SINGLE_CHAT_MESSAGE = 'single_chat_message';
+
+/**
+ * Sub Events
+ */
+
 const SUB_EVENT_RECEIVE_MESSAGE = 'receive_message';
-const SUB_EVENT_MESSAGE_FROM_SERVER = 'message_from_server';
 const SUB_EVENT_IS_USER_CONNECTED = 'is_user_connected';
 
-// status
-const  STATUS_MESSAGE_NOT_SENT = 1001;
-const STATUS_MESSAGE_SENT = 1002;
 
 const listen_port= process.env.PORT||3000;
+
 app.use('/',(req,res)=>{
 	res.send('Server is running');
 })
 
 server.listen(listen_port,()=>{console.log(`Server is running on ${listen_port}`)});
-
+/**
+ * declaring userMap
+ */
 const userMap=new Map();
+/**
+ * connection to socket @param {*} socket
+ */
 io.sockets.on(ON_CONNECTION, function (socket) {
 	onEachUserConnection(socket);
    });
@@ -42,16 +54,29 @@ io.sockets.on(ON_CONNECTION, function (socket) {
 	checkOnline(socket);
 	onDisconnected(socket);
 }
+/**
+ * sending message on socket
+ * @param {*} socket 
+ */
 const onMessage=(socket)=>{
 	socket.on(EVENT_SINGLE_CHAT_MESSAGE,(chat_message)=>{
 		singleMessageHandler(socket,chat_message);
 	});
 }
+/**
+ * check if user is online
+ * @param {*} socket 
+ */
 const checkOnline=(socket)=>{
 	socket.on(EVENT_IS_USER_ONLINE,(chat_user_details)=>{
 		onlineCheckHandler(socket,chat_user_details);
 	});
 }
+/**
+ * determining if user is online
+ * @param {*} socket 
+ * @param {Object} chat_user_details 
+ */
 const onlineCheckHandler=(socket,chat_user_details)=>{
 	let to_user_id=chat_user_details.to;
 	print(`Checking online User => ${to_user_id}` );
@@ -60,6 +85,11 @@ const onlineCheckHandler=(socket,chat_user_details)=>{
 	chat_user_details.to_user_online_status=isOnline;
 	sendBackToclient(socket,SUB_EVENT_IS_USER_CONNECTED,chat_user_details);
 }
+/**
+ * 
+ * @param {*} socket 
+ * @param {Object} chat_message 
+ */
 const singleMessageHandler=(socket,chat_message)=>{
 	print(`OnMessage: ${JSON.stringify(chat_message)}`);
 	let to_user_id=chat_message.to;
@@ -75,13 +105,36 @@ const singleMessageHandler=(socket,chat_message)=>{
 	chat_message.to_user_online_status=true;
 	sendToConnectedSocket(socket,to_user_socket_id,SUB_EVENT_RECEIVE_MESSAGE,chat_message);
 }
+
+/**
+ * get message from socket
+ * @param {*} socket 
+ * @param {String} event 
+ * @param {Object} chat_message 
+ */
+
  const sendBackToclient=(socket,event,chat_message)=>{
 	socket.emit(event,JSON.stringify(chat_message))
 
  }
+
+ /**
+  * send message to user connected on socket
+  * @param {*} socket 
+  * @param {String} to_user_socket_id 
+  * @param {String} event 
+  * @param {Object} chat_message 
+  */
+
 const sendToConnectedSocket=(socket,to_user_socket_id,event,chat_message)=>{
 	socket.to(`${to_user_socket_id}`).emit(event,JSON.stringify(chat_message));
 }
+
+/**
+ * getting socket id using receiver id
+ * @param {String} to_user_id 
+ */
+
 const getSocketIDFromMapForThisUser=(to_user_id)=>{
 	let userMapVal=userMap.get(`${to_user_id}`);
 	if(undefined==userMapVal){
@@ -89,6 +142,12 @@ const getSocketIDFromMapForThisUser=(to_user_id)=>{
 	}
 	return userMapVal.socket_id;
 }
+
+/**
+ * remove user from user map using socket id
+ * @param {String} socket_id 
+ */
+
 const removeUserWithSocketIdFromMap=(socket_id)=>{
 	print(`Deleting user from map :${socket_id}`);
 	let toDeleteUser;
@@ -105,6 +164,12 @@ const removeUserWithSocketIdFromMap=(socket_id)=>{
 	print(userMap);
 	printOnlineUsers();
 }
+
+/**
+ * Disconnect from socket
+ * @param {*} socket 
+ */
+
 const onDisconnected =(socket)=>{
   socket.on(ON_DISCONNECT,function(){
 	   print(`Disconnected ${socket.id}`);
@@ -115,12 +180,28 @@ const onDisconnected =(socket)=>{
   });
 }
 
+/**
+ * Add user to map to get total number of user on socket
+ * @param {String} key_user_id 
+ * @param {String} socket_id 
+ */
+
 const addUserToMap=(key_user_id,socket_id)=>{
 	userMap.set(key_user_id,socket_id)
 }
+
+/**
+ * print users who are online
+ */
+
 const printOnlineUsers=()=>{
 	print(`Online users: ${userMap.size}`);
 }
+
+/**
+ * Printing some text to Screen
+ * @param {String } txt 
+ */
 
 const print=(txt)=>{
 	console.log(txt);
